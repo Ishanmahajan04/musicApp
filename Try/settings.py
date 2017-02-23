41c6aa0ad249d5a0bbc4749161e2e9e9d877c8c3
ishanmahajan04 <ishan04mahajan@gmail.com>
2017-02-23 12:50:07 +0530

    music app setting changed

--- Try/settings.py
+++ Try/settings.py
@@ -119,7 +119,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-MEDIA_Root = os.path.join(BASE_DIR, 'media')
-MEDIA_URL='/media/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),)
+#
+# STATIC_URL = '/static/'
+#
+# MEDIA_Root = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL='/media/'
